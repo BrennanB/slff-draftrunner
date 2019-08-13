65dc6effddcfef5eb34994a3d08914f4883eb599
Brennan <brennan.bibic@gmail.com>
2019-08-13 06:04:54 -0400

    Fix bad grammar

--- templates.py
+++ templates.py
@@ -3,5 +3,5 @@ def new_draft(start_date, start_time, close_date, close_time, team_list, short_n
     new_draft = "Draft will be run on [date={} time={} timezone=\"America/Toronto\"] . " \
             "Signups close [date={} time={} timezone=\"America/Toronto\"] .\n\n# Draft Info\n\n" \
             "This is currently a semi-automated draft runner, and there may be bugs, timing will be 2/2/2.\n\n[details=\"Team List\"]\n{}\n[/details]\n\n" \
-            "Like this post to sign up, and send all the lists to my beautiful bot self! Make the title of your PM '{}'".format(start_date, start_time, close_date, close_time, team_list, short_name)
+            "Like this post to sign up, and send all the lists to my beautiful bot self! Make the title of your PM for lists '{}'".format(start_date, start_time, close_date, close_time, team_list, short_name)
     return new_draft
